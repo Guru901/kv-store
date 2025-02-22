ab64987d53fb19042ddd755ae3a324685b35b9ae
Guru901 <avinashkaur842@gmail.com>
2025-02-23 01:09:10 +0530

    Add jsonparser dependency and implement basic command handling in main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::env;
+use std::fs;
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    Command::new("touch data.json").spawn().unwrap();
+
+    let contents = fs::read_to_string("./test.json").unwrap();
+
+    let args = env::args().collect::<Vec<String>>();
+
+    let command = args.get(1).unwrap_or_else(|| {
+        println!("Usage: kv-store-json <command> <key> <value>");
+        println!("Commands:");
+        println!("  add <key> <value>   Add a key-value pair to the file");
+        println!("  get <key>           Get the value of a key");
+        std::process::exit(1);
+    });
 }
