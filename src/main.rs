da8d3f86312899b15a17c540648493011c3ae99e
Guru901 <avinashkaur842@gmail.com>
2025-02-23 01:10:01 +0530

    Implement JSON parsing and command handling for key-value operations

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::env;
 use std::fs;
 use std::process::Command;
 
+use jsonparser::JSONParser;
+
 fn main() {
     Command::new("touch data.json").spawn().unwrap();
 
@@ -16,4 +18,27 @@ fn main() {
         println!("  get <key>           Get the value of a key");
         std::process::exit(1);
     });
+
+    match JSONParser::from(&contents) {
+        Ok(json) => println!("{:#?}", json.get("hehe").unwrap()),
+        Err(e) => eprintln!("Failed to parse JSON: {}", e),
+    }
+
+    match command.as_str() {
+        "add" => {
+            let key = args.get(2).unwrap();
+            let value = args.get(3).unwrap();
+            // add this data to file
+        }
+        "get" => {
+            let key = args.get(2).unwrap();
+            // get this data from file
+        }
+        _ => {
+            println!("Usage: kv-store-json <command> <key> <value>");
+            println!("Commands:");
+            println!("  add <key> <value>   Add a key-value pair to the file");
+            println!("  get <key>           Get the value of a key");
+        }
+    }
 }
